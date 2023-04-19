b3c747da87173d26b97b46f2fea51dfc15abfe2c
PrachiBalpande <112539273+PrachiBalpande@users.noreply.github.com>
2023-04-19 19:56:47 +0530

    Update header.component.ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,3 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-header',
-//   templateUrl: './header.component.html',
-//   styleUrls: ['./header.component.css']
-// })
-// export class HeaderComponent {
-
-// }
-
-
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductService } from '../service/product.service';
